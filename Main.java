493d0331494978d0a9e043b12ab373c9f2859055
Mahant T <mahantvibisha@gmail.com>
2026-02-23 11:16:52 +0530

    Refactor Calculator class and improve menu display

--- Main.java
+++ Main.java
@@ -1,110 +1,157 @@
+
+
 import java.io.*;
 import java.util.*;
  class Calculator {
+
+	Scanner input = new Scanner(System.in);
+	boolean run = true;
+	int num1,num2;
+	double dnum1;
+	//Menu
+	void menu() {
+		String a[] = {"Addtion", "Subtract", "Multiplication", "Division", "Modulus","Square root" ,"Exit"};
+
+
 	
-		Scanner input=new Scanner(System.in);
-		    boolean run=true;
-		void menu(){
-		String a[]={"Add","Sub","Mul","Div","Mod","Exit"};
-        
-		
-		for(int i=0;i<=5;i++){
-		System.out.println("Press "+(i+1)+" : "+a[i]);
-		
-		}
+		int i=0;
+		for(String arr : a){
+	    System.out.println("Press " + (i + 1) + " : " + arr);
+	    i++;
+	}
 		System.out.print("Enter your choice:");
+	}
+	
+	
+	//Add method
+	int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+    //Subtract method
+	int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	//Multiplication method
+	int mul(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+    //Division method
+	float div(float num1, float num2) {
+		
+		return num1 / num2;
+
+	}
+	
+	
+    //Modulus method
+	int mod(int num1, int num2) {
+		return num1 % num2;
+	}
+	
+	//Square root
+	double sqt(double num1){
+	    return Math.sqrt(num1);
+	}
+
+    //Starting method
+	void start() {
+		
+		while (run) {
+			menu();
+			int choice = input.nextInt();
+			
+			if(choice>7){
+			  System.out.println("Invalid choice.Enter the number between 1 to 7");   
+			}
+			else{
+			if (choice == 7) {
+				System.out.println("Calculator closed...");
+				this.run = false;
+				break;
+			}
+			
+			if(choice==6){
+			  System.out.print("Enter a number :");
+			  dnum1=input.nextDouble();
+			  
+			}
+			else{
+			System.out.print("Enter a number 1 :");
+			 num1 = input.nextInt();
+			System.out.print("Enter a number 2 :");
+			 num2 = input.nextInt();
+			input.nextLine();
+			}
+
+			switch (choice) {
+			case 1:
+				System.out.println("Result :" + add(num1, num2));
+				break;
+			case 2:
+				System.out.println("Result :" + sub(num1, num2));
+				break;
+			case 3:
+				System.out.println("Result :" + mul(num1, num2));
+				break;
+			case 4:
+			if (num2 == 0) {
+			System.out.println("can't divide by 0");
+		
+		}else{
+				System.out.println("Result :" + div(num1, num2));
 		}
-		
-		int add(int num1,int num2){
-		   return num1+num2; 
-		}
-		
-		int sub(int num1,int num2){
-		   return num1-num2; 
-		}
-		
-		int mul(int num1,int num2){
-		   return num1*num2; 
-		}
-		
-		float div(float num1,float num2){
-		   if(num2==0){
-		      System.out.println("can't divide by 0");
-		      start(); 
-		      return 0;
-		   }
-		   return num1/num2; 
-		   
-		}
-		
-		int mod(int num1,int num2){
-		   return num1%num2; 
-		}
-		
-		
-		void start(){
-		    if(!run){
-		        return;
-		    }
-		    while(run){
-		        menu();
-		int choice=input.nextInt();
-		if(choice==6){
-		    System.out.println("Calculator closed...");
-		    run=false;
-		    break;
-		}
-		System.out.print("Enter a number 1 :");
-		int num1=input.nextInt();
-		System.out.print("Enter a number 2 :");
-		int num2=input.nextInt();
-		input.nextLine();
-		
-		switch(choice){
-    case 1:
-        System.out.println("Result :"+add(num1,num2));
-        break;
-    case 2:
-        System.out.println("Result :"+sub(num1,num2));
-        break;
-    case 3:
-        System.out.println("Result :"+mul(num1,num2));
-        break;
-    case 4:
-        System.out.println("Result :"+div(num1,num2));
-        break;
-    case 5:
-        System.out.println("Result :"+mod(num1,num2));
-        break;
-    default:
-        System.out.println("Invalid choice.Enter the number between 1 to 6");
+				break;
+			case 5:
+				System.out.println("Result :" + mod(num1, num2));
+				break;
+			case 6:
+			
+			if (dnum1 < 0) {
+    System.out.println("Cannot calculate square root of a negative number");
+} else {
+    System.out.println("Result :" + sqt(dnum1));
 }
-		end();
-		    }
+			break;
+			default:
+				System.out.println("Invalid choice.Enter the number between 1 to 7");
+				break;
+			}
+			end();
+			}
+			
+			System.out.println();
 		}
-		void end(){
-		    System.out.print("Want to do again (yes/no) :");
-		  
-		    String yrn=input.nextLine();
-		    if(yrn.equals("yes")){
-		       run=true;
-		       start();
-		    }
-		    else{
-		        
-		       System.out.println("Calculator closed...");
-		    run=false; 
-		        
-		    }
+	}
+	
+	//End method
+	void end() {
+		System.out.print("\nWant to do again (yes/no) :");
+
+		String yrn = input.next();
+		if (yrn.equals("yes")) {
+			this.run = true;
+		
+		} else {
+
+			System.out.println("\nCalculator closed...");
+			this.run = false;
+
 		}
+	}
+
+
+
+
+
+    
+    	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		calc.start();
 		
 
+	}
 }
 
-public class Main{
-    public static void main(String[] args){
-      Calculator calc=new Calculator();
-      calc.start();
-        
-    }
-}
